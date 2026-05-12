aea56fb57fd4d7d06f29cd4c95f5ddd799898c84
MrPanda78 <santidj28@gmail.com>
2026-05-12 16:09:28 -0300

    Added new suggested products while watching a product's details

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,15 +1,33 @@
 const products = require("../data/products.json");
 
-const interests = [
-    { name: 'Whiskey Blenders Pride 750ml', image: '/images/products/beverages/coca_cola_lata_220ml_pack_original_x8.webp', points: '19.900' },
-    { name: 'Nombre del producto o servicio', image: '/images/fondo_blanco_negro.webp', points: 'XX.XXX' },
-    { name: 'Nombre del producto o servicio', image: '/images/fondo_blanco_negro.webp', points: 'XX.XXX' },
-    { name: 'Nombre del producto o servicio', image: '/images/fondo_blanco_negro.webp', points: 'XX.XXX' }
-];
-
 const productController = {
     product: (req, res) => {
         const product = products[0];
+
+        // Filtra productos de la misma categoría para "interests"
+        const shuffledProducts = products.filter(p =>
+            p.category === product.category &&
+            p.id !== product.id // Evita incluir el mismo producto
+        );
+
+        // Recorre el array desde el último elemento hacia el primero para "interests"
+        for (let i = shuffledProducts.length - 1; i > 0; i--) {
+
+            // Genera una posición aleatoria entre 0 e i
+            const j = Math.floor(Math.random() * (i + 1));
+
+            // Intercambia el elemento actual con el elemento aleatorio
+            [shuffledProducts[i], shuffledProducts[j]] = [
+                shuffledProducts[j],
+                shuffledProducts[i]
+            ];
+        }
+
+        const interests = shuffledProducts.slice(0, 4).map(product => ({
+            ...product,
+            points: product.points.toLocaleString("es-AR"), // toLocaleString() sirve para formatear números según una configuración regional
+            image: product.image ? `/images/products/${product.category}/${product.image}` : "/images/fondo_blanco_negro.webp"
+        }));
         res.render('product', { loggedIn: 1, interests, product });
     }
 };
