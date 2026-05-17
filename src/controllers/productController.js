905dc67237ce7e7e88388a29c57519316198d052
Ornic03 <gianellamlazarini2@gmail.com>
2026-05-17 17:59:52 -0300

    Added normalizeId function

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,19 +1,35 @@
 const productsService = require("../services/productsService");
 const translateCategory = require("../utils/translateCategory");
+const normalizeId = require("../utils/normalizeId");
 
 const productController = {
     product: (req, res, next) => {
         try {
-            const id = Number(req.params.id);
+            const id = normalizeId(req.params.id);
+
+            if (!id)
+            {
+                return res.status(400).render("error400", {
+                    title: "Producto inválido - Ecommerce",
+                    loggedIn: 1,
+                    totalItems: 0,
+                    styles: [
+                        "/css/components/userDropdown.css",
+                        "/css/layouts/header.css",
+                        "/css/components/searchBar.css",
+                        "/css/components/cartAndProfile.css",
+                        "/css/views/errors.css",
+                        "/css/layouts/footer.css"
+                    ]
+                });
+            }
             const product = productsService.getProductById(id);
 
             if (!product) {
                 return res.status(404).render("error404");
             }
 
-            const interests = productsService.formatProducts(
-                productsService.getRelatedProducts(product, 4)
-            );
+            const interests = productsService.formatProducts(productsService.getRelatedProducts(product, 4));
             const cart = req.session.cart || [];
             const totalItems = cart.reduce((acc, item) => acc + item.quantity, 0);
 
